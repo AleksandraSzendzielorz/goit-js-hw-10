ae45e123b9bc5b27d6aabc7d7a93f7cc6dac0a49
AleksandraSzendzielorz <kontaktowyoficjalny@gmail.com>
2024-02-20 16:26:35 +0100

    commit

---
+++ src/cat-api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+export const selectInput = document.querySelector('.breed-select');
+const cats = [];
+
+export const fetchBreeds = () => {
+  axios.defaults.headers.common['x-api-key'] =
+    'llive_2gXZeTO4QxkVjYIeOt16J1eoABZswimOwdEjUEgCKRyt4MCSUoAfuLevzAgoHDrB';
+
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      response.data.map(({ id, name }) => {
+        cats.push({ text: name, value: id });
+      });
+      return cats;
+    })
+    .catch(() => {
+      Notiflix.Notify.failure('Something went wrong. Try reloading the page!');
+    });
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(() => {
+      Notiflix.Notify.failure('Something went wrong. Try reloading the page!');
+    });
+};
